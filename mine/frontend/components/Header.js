b5e56859720a0c316bef2cf60e392125c252c785
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-01 15:38:55 -0500

    Videos 1-4

---
+++ mine/frontend/components/Header.js
@@ -0,0 +1,14 @@
+import Nav from "./Nav";
+const Header = () => (
+  <div>
+    <div class="bar">
+      <a href="">Sick Fits</a>
+      <Nav />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+);
+export default Header;
