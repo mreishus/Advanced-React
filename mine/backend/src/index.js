8fac7ead99b39212c111507cd874836ed457fde3
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-02 13:41:49 -0500

    Finish video 12
    "Getting our GraphQL Yoga Server Running"
    
    Very confusing.

--- mine/backend/src/index.js
+++ mine/backend/src/index.js
@@ -1,1 +1,25 @@
-// let's go!
+const result = require("dotenv").config({ path: "./variables.env" });
+if (result.error) {
+  throw result.error;
+}
+//console.log(result.parsed);
+
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO Use express middleware for cookies (JWT)
+// TODO Use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  }
+);
