82ad69787cad9af8aac07fed35d6371bd7877951
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-04 16:11:52 -0500

    Finished vid 26 (currently logged in user)

--- mine/backend/src/index.js
+++ mine/backend/src/index.js
@@ -1,4 +1,6 @@
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
 const result = require("dotenv").config({ path: "./variables.env" });
 if (result.error) {
   throw result.error;
@@ -12,7 +14,16 @@ const server = createServer();
 
 // Use express middleware for cookies (JWT)
 server.express.use(cookieParser());
-// TODO Use express middleware to populate current user
+
+// Use express middleware to populate current user
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
--- mine/backend/src/resolvers/Query.js
+++ mine/backend/src/resolvers/Query.js
@@ -3,11 +3,21 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   items: forwardTo("db"),
   item: forwardTo("db"),
-  itemsConnection: forwardTo("db")
-  // async items(parent, args, ctx, info) {
-  //   const items = ctx.db.query.items();
-  //   return items;
-  // }
+  itemsConnection: forwardTo("db"),
+  me(parent, args, ctx, info) {
+    // check if there is a current user id
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: {
+          id: ctx.request.userId
+        }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
