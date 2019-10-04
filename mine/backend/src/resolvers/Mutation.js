ec9c6654ca662ac94905ba36a299e565bb91ebf1
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-04 16:43:33 -0500

    Vid 28, signout

--- mine/backend/src/resolvers/Mutation.js
+++ mine/backend/src/resolvers/Mutation.js
@@ -91,6 +91,10 @@ const Mutations = {
     });
     // return user
     return user;
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie("token");
+    return { message: "Goodbye!" };
   }
 };
 
