9cb52089da64f63c25bde2972ea57b34cc9e8b62
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-05 10:17:03 -0500

    Vid 30, frontend password reset

--- mine/frontend/pages/signup.js
+++ mine/frontend/pages/signup.js
@@ -1,5 +1,6 @@
 import Signup from "../components/Signup";
 import Signin from "../components/Signin";
+import RequestReset from "../components/RequestReset";
 import styled from "styled-components";
 const Columns = styled.div`
   display: grid;
@@ -10,6 +11,7 @@ const SignupPage = props => (
   <Columns>
     <Signup />
     <Signin />
+    <RequestReset />
   </Columns>
 );
 export default SignupPage;
